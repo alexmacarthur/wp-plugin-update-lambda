45c75c60219b227b4717b3f589ec74ae9a886e7d
Alex MacArthur <alex.macarthur@daveramsey.com>
2020-05-03 22:20:00 -0500

    wip.

--- api/wp-update.ts
+++ api/wp-update.ts
@@ -1,5 +1,8 @@
+import pluginConfig from "./_packages/wp-typeit.json";
 import { NowRequest, NowResponse } from '@now/node';
 
 export default (req: NowRequest, res: NowResponse) => {
-  res.json({ name: 'Alex', email: 'alex@macarthur.me' })
+
+  res.json(pluginConfig);
+  // res.json({ name: 'Alex', email: 'alex@macarthur.me' })
 }
