f3d99e1c300c7b97cd79ea60227158ec169e9e16
Alex MacArthur <alex.macarthur@daveramsey.com>
2020-05-19 23:01:28 -0500

    Make more flexible.

--- api/wp-update.ts
+++
@@ -1,8 +0,0 @@
-import pluginConfig from "./_packages/wp-typeit.json";
-import { NowRequest, NowResponse } from '@now/node';
-
-export default (req: NowRequest, res: NowResponse) => {
-
-  res.json(pluginConfig);
-  // res.json({ name: 'Alex', email: 'alex@macarthur.me' })
-}
